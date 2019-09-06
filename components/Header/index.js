1e78703dae2fa6f6a45714d725ff606f2f7812a5
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-06 08:33:42 +0100

    created a header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+    const headerDiv = document.createElement('div');
+    headerDiv.classList.add('header');
+
+    const date = document.createElement('span');
+    date.classList.add('date');
+    date.textContent = `MARCH 28, 2019 `
+
+    const myH1 = document.createElement('h1');
+    myH1.textContent = `Lambda Times`
+
+    const temperature = document.createElement('span');
+    temperature.classList.add('temp');
+    temperature.textContent = `98°`
+
+    headerDiv.appendChild(date); //we append the date into the header cntainer
+    headerDiv.appendChild(myH1);  // we append the myH1 into the header cntainer
+    headerDiv.appendChild(temperature);  //we append the temperature into the header cntainer
+
+    return headerDiv  //since the header container is the one carrying the children we return it/
+}
+
+let newHeader = Header(); //we create a new header with the headerContainer
+
+let headerContainer = document.querySelector('.header-container');
+
+headerContainer.appendChild(newHeader); //we add it to the ".headerContainer"
